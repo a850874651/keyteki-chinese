3f53816115a99c94fd17e91cf466b29235d131fa
wjxzz <850874651@qq.com>
2026-02-27 12:37:39 +0800

    上次更新未保存的提交

--- client/Components/GameBoard/CardImage.jsx
+++ client/Components/GameBoard/CardImage.jsx
@@ -11,6 +11,9 @@ import './CardImage.scss';
 const CardImage = ({ card, cardBack, size, halfSize, onMouseOver, onMouseOut }) => {
     const { i18n } = useTranslation();
     const fabricRef = useRef();
+    
+    // 添加 renderIdRef
+    const renderIdRef = useRef(0);
 
     const ref = useCallback(
         async (node) => {
@@ -62,19 +65,20 @@ const CardImage = ({ card, cardBack, size, halfSize, onMouseOver, onMouseOut }) 
                         }
                     }
                 }
+                return;
             }
-            return;
-        }
 
-        if (!fabricRef.current) {
-            try {
-                fabricRef.current = new fabric.StaticCanvas(node);
-                fabricRef.current.renderOnAddRemove = false;
-            } catch {
-                fabricRef.current = null;
+            if (!fabricRef.current) {
+                try {
+                    fabricRef.current = new fabric.StaticCanvas(node);
+                    fabricRef.current.renderOnAddRemove = false;
+                } catch {
+                    fabricRef.current = null;
+                }
             }
-        }
-    }, []);
+        },
+        [card, halfSize, i18n.language, size]
+    );
 
     useEffect(() => {
         const canvas = fabricRef.current;
@@ -95,7 +99,6 @@ const CardImage = ({ card, cardBack, size, halfSize, onMouseOver, onMouseOut }) 
                     ...card,
                     size,
                     halfSize,
-                    showAccolades,
                     url
                 });
             } catch {
@@ -107,41 +110,9 @@ const CardImage = ({ card, cardBack, size, halfSize, onMouseOver, onMouseOut }) 
             }
         })();
     }, [
-        card?.id,
-        card?.location,
-        card?.modifiedPower,
-        card?.tokens && card.tokens.amber,
-        card?.tokens && card.tokens.armor,
-        card?.tokens && card.tokens.awakening,
-        card?.tokens && card.tokens.damage,
-        card?.tokens && card.tokens.depth,
-        card?.tokens && card.tokens.disruption,
-        card?.tokens && card.tokens.doom,
-        card?.tokens && card.tokens.enrage,
-        card?.tokens && card.tokens.fuse,
-        card?.tokens && card.tokens.glory,
-        card?.tokens && card.tokens.growth,
-        card?.tokens && card.tokens.ignorance,
-        card?.tokens && card.tokens.knowledge,
-        card?.tokens && card.tokens.mutation,
-        card?.tokens && card.tokens.power,
-        card?.tokens && card.tokens.scheme,
-        card?.tokens && card.tokens.time,
-        card?.tokens && card.tokens.ward,
-        card?.tokens && card.tokens.warrant,
-        card?.tokens && card.tokens.yea,
-        card?.tokens && card.tokens.nay,
-        card?.tokens && card.tokens.wisdom,
-        card?.tokens && card.tokens.hatch,
-        card?.tokens && card.tokens.paint,
-        card?.tokens && card.tokens.trade,
-        card?.tokens && card.tokens.stun,
-        card?.pseudoDamage,
-        card?.wardBroken,
-        card?.facedown,
+        card,
         size,
         halfSize,
-        showAccolades,
         i18n.language
     ]);
 
@@ -167,7 +138,7 @@ const CardImage = ({ card, cardBack, size, halfSize, onMouseOver, onMouseOut }) 
             }
             onMouseOut={onMouseOut}
             className='h-100 w-100'
-            ref={setCanvasRef}
+            ref={ref}
         />
     );
 };
