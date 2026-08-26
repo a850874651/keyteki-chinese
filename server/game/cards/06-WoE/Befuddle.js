6b5fe3269054ec3dbf831af688c913bf0b22b342
wjxzz <850874651@qq.com>
2026-08-26 19:41:21 +0800

    翻译问题修复

--- server/game/cards/06-WoE/Befuddle.js
+++ server/game/cards/06-WoE/Befuddle.js
@@ -9,7 +9,7 @@ class Befuddle extends Card {
                 mode: 'house',
                 houses: (context) => context.player.opponent.houses
             },
-            effect: '{1} 无法打出 {2} 势力的卡牌',
+            effect: '{1} 无法打出 {2} 势力以外的卡牌',
             effectArgs: (context) => [context.player.opponent, context.house],
             effectAlert: true,
             gameAction: ability.actions.duringOpponentNextTurn((context) => ({
